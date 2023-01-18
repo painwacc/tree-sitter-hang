ce66503ae68bbe74a7a82d8deaceb0dfad08f094
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2023-01-18 01:31:06 +0000

    Minimize to just one rule

--- grammar.js
+++ grammar.js
@@ -1,33 +1,8 @@
 module.exports = grammar({
     name: 'wacc',
 
-    extras: $ => [
-        $.line_comment,
-        /\s/,
-    ],
-
-    // word: $ => $.ident,
-
-    // Order must match TokenType in scanner.cc
-    // externals: $ => [$.ident],
 
     rules: {
-        source_file: $ => $.stat,
-        type: $ => 'string',
-
-        /*
-         * Expressions
-         */
-        expr: $ => /"([[\x00-\x7f]&&[^\\'"\n]]|(\\[0btnfr"'\\]))*"/,
-
-
-
-
-
-        ident: $ => 's',
-
-        stat: $ => seq($.type, field('ident', $.ident), '=', $.expr),
-
-        line_comment: $ => /#[^\n\r]*[\n\r]*/
+        source_file: $ => /"([[\x00-\x7f]&&[^\\'"\n]]|(\\[0btnfr"'\\]))*"/,
     }
 });
--- src/parser.c
+++ src/parser.c
@@ -6,46 +6,31 @@
 #endif
 
 #define LANGUAGE_VERSION 14
-#define STATE_COUNT 8
+#define STATE_COUNT 4
 #define LARGE_STATE_COUNT 2
-#define SYMBOL_COUNT 8
+#define SYMBOL_COUNT 3
 #define ALIAS_COUNT 0
-#define TOKEN_COUNT 6
+#define TOKEN_COUNT 2
 #define EXTERNAL_TOKEN_COUNT 0
-#define FIELD_COUNT 1
-#define MAX_ALIAS_SEQUENCE_LENGTH 4
-#define PRODUCTION_ID_COUNT 2
+#define FIELD_COUNT 0
+#define MAX_ALIAS_SEQUENCE_LENGTH 1
+#define PRODUCTION_ID_COUNT 1
 
 enum {
-  sym_type = 1,
-  sym_expr = 2,
-  sym_ident = 3,
-  anon_sym_EQ = 4,
-  sym_line_comment = 5,
-  sym_source_file = 6,
-  sym_stat = 7,
+  aux_sym_source_file_token1 = 1,
+  sym_source_file = 2,
 };
 
 static const char * const ts_symbol_names[] = {
   [ts_builtin_sym_end] = "end",
-  [sym_type] = "type",
-  [sym_expr] = "expr",
-  [sym_ident] = "ident",
-  [anon_sym_EQ] = "=",
-  [sym_line_comment] = "line_comment",
+  [aux_sym_source_file_token1] = "source_file_token1",
   [sym_source_file] = "source_file",
-  [sym_stat] = "stat",
 };
 
 static const TSSymbol ts_symbol_map[] = {
   [ts_builtin_sym_end] = ts_builtin_sym_end,
-  [sym_type] = sym_type,
-  [sym_expr] = sym_expr,
-  [sym_ident] = sym_ident,
-  [anon_sym_EQ] = anon_sym_EQ,
-  [sym_line_comment] = sym_line_comment,
+  [aux_sym_source_file_token1] = aux_sym_source_file_token1,
   [sym_source_file] = sym_source_file,
-  [sym_stat] = sym_stat,
 };
 
 static const TSSymbolMetadata ts_symbol_metadata[] = {
@@ -53,52 +38,14 @@ static const TSSymbolMetadata ts_symbol_metadata[] = {
     .visible = false,
     .named = true,
   },
-  [sym_type] = {
-    .visible = true,
-    .named = true,
-  },
-  [sym_expr] = {
-    .visible = true,
-    .named = true,
-  },
-  [sym_ident] = {
-    .visible = true,
-    .named = true,
-  },
-  [anon_sym_EQ] = {
-    .visible = true,
+  [aux_sym_source_file_token1] = {
+    .visible = false,
     .named = false,
-  },
-  [sym_line_comment] = {
-    .visible = true,
-    .named = true,
   },
   [sym_source_file] = {
     .visible = true,
     .named = true,
   },
-  [sym_stat] = {
-    .visible = true,
-    .named = true,
-  },
-};
-
-enum {
-  field_ident = 1,
-};
-
-static const char * const ts_field_names[] = {
-  [0] = NULL,
-  [field_ident] = "ident",
-};
-
-static const TSFieldMapSlice ts_field_map_slices[PRODUCTION_ID_COUNT] = {
-  [1] = {.index = 0, .length = 1},
-};
-
-static const TSFieldMapEntry ts_field_map_entries[] = {
-  [0] =
-    {field_ident, 1},
 };
 
 static const TSSymbol ts_alias_sequences[PRODUCTION_ID_COUNT][MAX_ALIAS_SEQUENCE_LENGTH] = {
@@ -114,13 +61,9 @@ static const TSStateId ts_primary_state_ids[STATE_COUNT] = {
   [1] = 1,
   [2] = 2,
   [3] = 3,
-  [4] = 4,
-  [5] = 5,
-  [6] = 6,
-  [7] = 7,
 };
 
-static inline bool sym_expr_character_set_1(int32_t c) {
+static inline bool aux_sym_source_file_token1_character_set_1(int32_t c) {
   return (c < 'b'
     ? (c < '0'
       ? (c < '\''
@@ -139,86 +82,28 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   eof = lexer->eof(lexer);
   switch (state) {
     case 0:
-      if (eof) ADVANCE(10);
+      if (eof) ADVANCE(3);
       if (lookahead == '"') ADVANCE(1);
-      if (lookahead == '#') ADVANCE(17);
-      if (lookahead == '=') ADVANCE(15);
-      if (lookahead == 's') ADVANCE(14);
       if (lookahead == '\t' ||
           lookahead == '\n' ||
           lookahead == '\r' ||
           lookahead == ' ') SKIP(0)
       END_STATE();
     case 1:
-      if (lookahead == '"') ADVANCE(12);
-      if (lookahead == '\\') ADVANCE(9);
+      if (lookahead == '"') ADVANCE(4);
+      if (lookahead == '\\') ADVANCE(2);
       if ((0 <= lookahead && lookahead <= '\t') ||
           (11 <= lookahead && lookahead <= '&') ||
           ('(' <= lookahead && lookahead <= 127)) ADVANCE(1);
       END_STATE();
     case 2:
-      if (lookahead == '#') ADVANCE(17);
-      if (lookahead == 's') ADVANCE(8);
-      if (lookahead == '\t' ||
-          lookahead == '\n' ||
-          lookahead == '\r' ||
-          lookahead == ' ') SKIP(2)
+      if (aux_sym_source_file_token1_character_set_1(lookahead)) ADVANCE(1);
       END_STATE();
     case 3:
-      if (lookahead == '#') ADVANCE(17);
-      if (lookahead == 's') ADVANCE(13);
-      if (lookahead == '\t' ||
-          lookahead == '\n' ||
-          lookahead == '\r' ||
-          lookahead == ' ') SKIP(3)
-      END_STATE();
-    case 4:
-      if (lookahead == 'g') ADVANCE(11);
-      END_STATE();
-    case 5:
-      if (lookahead == 'i') ADVANCE(6);
-      END_STATE();
-    case 6:
-      if (lookahead == 'n') ADVANCE(4);
-      END_STATE();
-    case 7:
-      if (lookahead == 'r') ADVANCE(5);
-      END_STATE();
-    case 8:
-      if (lookahead == 't') ADVANCE(7);
-      END_STATE();
-    case 9:
-      if (sym_expr_character_set_1(lookahead)) ADVANCE(1);
-      END_STATE();
-    case 10:
       ACCEPT_TOKEN(ts_builtin_sym_end);
       END_STATE();
-    case 11:
-      ACCEPT_TOKEN(sym_type);
-      END_STATE();
-    case 12:
-      ACCEPT_TOKEN(sym_expr);
-      END_STATE();
-    case 13:
-      ACCEPT_TOKEN(sym_ident);
-      END_STATE();
-    case 14:
-      ACCEPT_TOKEN(sym_ident);
-      if (lookahead == 't') ADVANCE(7);
-      END_STATE();
-    case 15:
-      ACCEPT_TOKEN(anon_sym_EQ);
-      END_STATE();
-    case 16:
-      ACCEPT_TOKEN(sym_line_comment);
-      if (lookahead == '\n' ||
-          lookahead == '\r') ADVANCE(16);
-      END_STATE();
-    case 17:
-      ACCEPT_TOKEN(sym_line_comment);
-      if (lookahead == '\n' ||
-          lookahead == '\r') ADVANCE(16);
-      if (lookahead != 0) ADVANCE(17);
+    case 4:
+      ACCEPT_TOKEN(aux_sym_source_file_token1);
       END_STATE();
     default:
       return false;
@@ -227,85 +112,42 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
 
 static const TSLexMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
-  [1] = {.lex_state = 2},
-  [2] = {.lex_state = 3},
+  [1] = {.lex_state = 0},
+  [2] = {.lex_state = 0},
   [3] = {.lex_state = 0},
-  [4] = {.lex_state = 0},
-  [5] = {.lex_state = 0},
-  [6] = {.lex_state = 0},
-  [7] = {.lex_state = 0},
 };
 
 static const uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
   [0] = {
     [ts_builtin_sym_end] = ACTIONS(1),
-    [sym_type] = ACTIONS(1),
-    [sym_expr] = ACTIONS(1),
-    [sym_ident] = ACTIONS(1),
-    [anon_sym_EQ] = ACTIONS(1),
-    [sym_line_comment] = ACTIONS(3),
+    [aux_sym_source_file_token1] = ACTIONS(1),
   },
   [1] = {
     [sym_source_file] = STATE(3),
-    [sym_stat] = STATE(4),
-    [sym_type] = ACTIONS(5),
-    [sym_line_comment] = ACTIONS(3),
+    [aux_sym_source_file_token1] = ACTIONS(3),
   },
 };
 
 static const uint16_t ts_small_parse_table[] = {
-  [0] = 2,
-    ACTIONS(3), 1,
-      sym_line_comment,
+  [0] = 1,
+    ACTIONS(5), 1,
+      ts_builtin_sym_end,
+  [4] = 1,
     ACTIONS(7), 1,
-      sym_ident,
-  [7] = 2,
-    ACTIONS(3), 1,
-      sym_line_comment,
-    ACTIONS(9), 1,
-      ts_builtin_sym_end,
-  [14] = 2,
-    ACTIONS(3), 1,
-      sym_line_comment,
-    ACTIONS(11), 1,
-      ts_builtin_sym_end,
-  [21] = 2,
-    ACTIONS(3), 1,
-      sym_line_comment,
-    ACTIONS(13), 1,
-      anon_sym_EQ,
-  [28] = 2,
-    ACTIONS(3), 1,
-      sym_line_comment,
-    ACTIONS(15), 1,
-      sym_expr,
-  [35] = 2,
-    ACTIONS(3), 1,
-      sym_line_comment,
-    ACTIONS(17), 1,
       ts_builtin_sym_end,
 };
 
 static const uint32_t ts_small_parse_table_map[] = {
   [SMALL_STATE(2)] = 0,
-  [SMALL_STATE(3)] = 7,
-  [SMALL_STATE(4)] = 14,
-  [SMALL_STATE(5)] = 21,
-  [SMALL_STATE(6)] = 28,
-  [SMALL_STATE(7)] = 35,
+  [SMALL_STATE(3)] = 4,
 };
 
 static const TSParseActionEntry ts_parse_actions[] = {
   [0] = {.entry = {.count = 0, .reusable = false}},
   [1] = {.entry = {.count = 1, .reusable = false}}, RECOVER(),
-  [3] = {.entry = {.count = 1, .reusable = true}}, SHIFT_EXTRA(),
-  [5] = {.entry = {.count = 1, .reusable = true}}, SHIFT(2),
-  [7] = {.entry = {.count = 1, .reusable = true}}, SHIFT(5),
-  [9] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
-  [11] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_source_file, 1),
-  [13] = {.entry = {.count = 1, .reusable = true}}, SHIFT(6),
-  [15] = {.entry = {.count = 1, .reusable = true}}, SHIFT(7),
-  [17] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_stat, 4, .production_id = 1),
+  [3] = {.entry = {.count = 1, .reusable = true}}, SHIFT(2),
+  [5] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_source_file, 1),
+  [7] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
 };
 
 #ifdef __cplusplus
@@ -332,9 +174,6 @@ extern const TSLanguage *tree_sitter_wacc(void) {
     .small_parse_table_map = ts_small_parse_table_map,
     .parse_actions = ts_parse_actions,
     .symbol_names = ts_symbol_names,
-    .field_names = ts_field_names,
-    .field_map_slices = ts_field_map_slices,
-    .field_map_entries = ts_field_map_entries,
     .symbol_metadata = ts_symbol_metadata,
     .public_symbol_map = ts_symbol_map,
     .alias_map = ts_non_terminal_alias_map,
